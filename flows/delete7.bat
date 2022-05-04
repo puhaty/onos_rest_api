d2ebe34a75404ae4446db68630e25604507e7a89
mchechlo <m.chechlowski@stud.elka.pw.edu.pl>
2022-05-04 20:17:43 +0200

    function post_flow implementation

--- flows/delete7.bat
+++
@@ -1,3 +0,0 @@
-curl --user onos:rocks -X DELETE "http://192.168.56.101:8181/onos/v1/flows/of:0000000000000002/49539597479875857
-"
-pause
--- flows/switch4.bat
+++
@@ -1,9 +0,0 @@
-
-curl --user onos:rocks -X POST "http://192.168.56.101:8181/onos/v1/flows/of:0000000000000004" -d @switch4.json -H "Content-Type: application/json" -H "Accept: application/json"
-curl --user onos:rocks -X POST "http://192.168.56.101:8181/onos/v1/flows/of:0000000000000004" -d @switch4_2.json -H "Content-Type: application/json" -H "Accept: application/json"
-curl --user onos:rocks -X POST "http://192.168.56.101:8181/onos/v1/flows/of:0000000000000004" -d @switch4_3.json -H "Content-Type: application/json" -H "Accept: application/json"
-curl --user onos:rocks -X POST "http://192.168.56.101:8181/onos/v1/flows/of:0000000000000004" -d @switch4_4.json -H "Content-Type: application/json" -H "Accept: application/json"
-curl --user onos:rocks -X POST "http://192.168.56.101:8181/onos/v1/flows/of:0000000000000004" -d @switch4_5.json -H "Content-Type: application/json" -H "Accept: application/json"
-curl --user onos:rocks -X POST "http://192.168.56.101:8181/onos/v1/flows/of:0000000000000004" -d @switch4_6.json -H "Content-Type: application/json" -H "Accept: application/json"
-curl --user onos:rocks -X POST "http://192.168.56.101:8181/onos/v1/flows/of:0000000000000004" -d @switch4_7.json -H "Content-Type: application/json" -H "Accept: application/json"
-pause
