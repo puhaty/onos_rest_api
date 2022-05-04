318a25fabb11ffb202f3fea56c0daee8940084c3
mchechlo <m.chechlowski@stud.elka.pw.edu.pl>
2022-05-04 20:41:05 +0200

    function post_flow implementation

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from routing import the_shorthest_path
 
 if __name__ == '__main__':
-    the_shorthest_path("10.0.0.1", "10.0.0.10")
+    print("Enter first host address:")
+    h1 = input()
+    print("Enter dst host address:")
+    h2 = input()
+    the_shorthest_path(h1, h2)
